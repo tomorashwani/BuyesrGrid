698e4b36238ca43c73ea9bbb601ea1ce20b81b1c
Ashwani tomar <ashwani_tomar@mohawkind.com>
2020-11-20 08:08:10 +0000

    Commmmited on 20/11/2020

---
+++ buyersgrid/webapp/controller/App.controller.js
@@ -0,0 +1,50 @@
+sap.ui.define([
+	"./BaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/Fragment"
+], function (BaseController, JSONModel, Fragment) {
+	"use strict";
+
+	return BaseController.extend("cf.buyersgrid.controller.App", {
+
+		onInit: function () {
+			var oViewModel,
+				fnSetAppNotBusy,
+				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
+
+			this.user = 'Ashley Nalley';
+			var that = this;
+			//console.log(this);
+
+			oViewModel = new JSONModel({
+				busy: true,
+				delay: 0,
+				layout: "OneColumn",
+				previousLayout: "",
+				actionButtonsInfo: {
+					midColumn: {
+						fullScreen: false
+					}
+				}
+			});
+			this.setModel(oViewModel, "appView");
+
+			fnSetAppNotBusy = function () {
+				oViewModel.setProperty("/busy", false);
+				oViewModel.setProperty("/delay", iOriginalBusyDelay);
+			};
+
+			// apply content density mode to root view
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+
+		/*	var localData = this.getOwnerComponent().getModel("localData").getData();
+			var lModel = new JSONModel(localData);
+			this.getView().setModel(lModel, "localModel");*/
+		},
+
+		onAfterRendering: function () {
+		},
+
+
+	});
+});
---
+++ buyersgrid/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"cf/buyersgrid/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
